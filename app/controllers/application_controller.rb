0c8e0286ed26b1919cf51bb8fe9cc560cd53216b
macrocro <me@macrocro.com>
2014-01-13 15:46:50 +0900

    # shop_idのpref,cityにURLをあわせるようredirect と not_foundメソッドを作った

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+ def not_found!
+   # render "errors/not_found", layout: "errors", status: 404
+   render "errors/not_found", layout: "application", status: 404
+ end
+
 end
--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -22,12 +22,25 @@ class ShopController < ApplicationController
   end
 
   def data
-    @shop = Shop.find_by_id params[:id]
-    @pref_shops = Shop.where('pref = ? and city = ?', params[:pref], params[:city])
+    @shop = Shop.find_by_id data_params[:id]
+    @pref_shops = Shop.where('pref = ? and city = ?', data_params[:pref], data_params[:city])
+
+    return not_found! unless @shop && @pref_shops.present?
+
+    # shop_idのpref,cityにURLをあわせるようredirect
+    if !(@shop[:pref] == data_params[:pref] && @shop[:city] == data_params[:city].to_i)
+      return redirect_to shop_data_path(@shop[:pref], @shop[:city], @shop[:id])
+    end
 
     add_breadcrumb @shop.ja_pref, shop_pref_path(params[:pref])
     add_breadcrumb @shop.ja_city, shop_city_path(params[:pref], params[:city])
     add_breadcrumb @shop.name
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def data_params
+    params.permit(:pref, :city, :id)
   end
 
 end
