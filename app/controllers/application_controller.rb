4484e2ad23559ae7e21973e21024766301a9d44e
macrocro <me@macrocro.com>
2014-01-16 02:50:29 +0900

    ogpの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     @meta = {
       :site => 'すぐもぐ！',
       # :image => URI.join(root_url, view_context.image_path('camera.png')),
-      :image => view_context.image_path('tenbosu.jpg'),
+      :image => URI.join(root_url,view_context.image_path('tenbosu.jpg')),
       :description => 'すぐに潜れる、ダイビングショップ検索サイト。すべての人にダイビングを！',
       :keywords => 'ダイビング, 体験ダイビング, スキューバダイビング',
       :top_canonical => 'http://sugumogu.com',
